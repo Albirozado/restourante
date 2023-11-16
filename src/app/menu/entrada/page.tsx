d1d542420a6d68488264735f55431c44a67a2d00
Albirozado <albirosaldoalfredo12@gmail.com>
2023-11-16 16:45:06 +0200

    carregando feacture

--- src/app/menu/entrada/page.tsx
+++ src/app/menu/entrada/page.tsx
@@ -11,17 +11,26 @@ export default async function Entradapage(){
         
         <section className="w-[1000px] max-w-full mx-auto">
             <div className="grid grid-cols-1 gap-2 sm:gap-4 sm:grid-cols-2  pb-3">
-            {data.map((en: any)=>(
-                <div className="flex flex-row justify-between flex-1 p-2 rounded-sm shadow-sm " style={{border: "solid 1px rgba(0,0,0,.12)"}} key={en._id}>
-                    <MenuCompo
-                    nome = {en.pratonome}
-                    img = {en.pratoimage}
+                {data.length === 0 && data.length < 1 ? 
+                <div><h1 className="text-5xl">CARREGANDOOOOOO.....</h1></div>
+                :
+                <>
+                         {data.map((en: any)=>(
                     
-                    />
+                    <div className="flex flex-row justify-between flex-1 p-2 rounded-sm shadow-sm " style={{border: "solid 1px rgba(0,0,0,.12)"}} key={en._id}>
+                        <MenuCompo
+                        nome = {en.pratonome}
+                        img = {en.pratoimage}
+                        
+                        />
 
-                </div>
+                    </div>
+            
     
-            ))}
+                        ))}
+                </>
+              
+                }
                 
             </div>
 
